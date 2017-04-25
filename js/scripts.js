c7e4537a90ce45e6b80fc28a7967ae59ee12703e
Beth Hansen and Jenna Cooper <beth@>
2017-04-25 15:16:00 -0700

    Add toggler animation effect to a paragraph -and other stuff

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,51 @@ $(document).ready(function() {
       $(".unicorn-hidden").toggle();
       $(".unicorn").toggle();
       });
+
+
+$("button#green").click(function() {
+  // $("body").removeClass();
+   $("body").toggleClass("green-background");
+ });
+
+ // $("button#purple").click(function() {
+ //   // $("body").removeClass();
+ //    $("body").toggleClass("magic");
+ //  });
+
+
+$("button#yellow").click(function() {
+  // $("body").removeClass();
+   $("body").toggleClass("yellow-background");
+ });
+
+$("button#red").click(function() {
+  // $("body").removeClass();
+   $("body").toggleClass("red-background");
+});
+$("button#pink").click(function() {
+  // $("body").removeClass();
+   $("body").toggleClass("pink-background");
+});
+
+$( function() {
+  var state = true;
+  $( "#button" ).on( "click", function() {
+    if ( state ) {
+      $( "#effect" ).animate({
+        backgroundColor: "#aa0000",
+        color: "#fff",
+        width: 500
+      }, 1000 );
+    } else {
+      $( "#effect" ).animate({
+        backgroundColor: "#fff",
+        color: "#000",
+        width: 240
+      }, 1000 );
+    }
+    state = !state;
+  });
+} );
+
 });
